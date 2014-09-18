d79d6e4d4f4708bbe29a0bf864756c6a767a377f
Odarchenko N.D <odarchenko.n.d@gmail.com>
2014-09-18 19:25:57 +0300

    init

---
+++ app.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import StringIO
+
+
+__author__ = 'developer'
+
+import os
+import web
+import pickle
+import base64
+import random
+from datetime import datetime
+from PIL import Image
+
+
+urls = (
+    '/', 'Index',
+    '/teach', 'Teach',
+    '/(.+)', 'StaticFiles'
+)
+
+
+class Index:
+    def GET(self):
+        with open('static/index.html', 'rb') as rb:
+            html = rb.read()
+        return html.replace('ip.gif', random.choice(os.listdir('static/images-learn')))
+
+    def POST(self):
+        img_str = web.input(name=['image']).get('image', 'data:image/png;base64,')
+        filename_src = web.input(name=['file']).get('file', None)
+        if not (filename_src and img_str):
+            return u'Ошибка'
+        img_data = base64.b64decode(img_str.replace('data:image/png;base64,', ''))
+        im = Image.open(StringIO.StringIO(img_data))
+        #im = Image.frombytes('RGB', (100, 100), data)
+        #im = Image.open(base64.b64decode(img_str.replace('data:image/png;base64,', '')))
+        data = [int(px != (0, 0, 0, 0)) for px in iter(im.getdata())]
+        with open('trained.net', 'r') as file_object:
+            net = pickle.load(file_object)
+        # каталог с изображениями для обучения
+        src = 'static/images-learn'
+        files_known = os.listdir(src)
+        # соответсвия между именем распозноваемого файла и цифрой на выходе из сети
+        codes = dict()
+        # цифра на выходе
+        num = 0
+        # перебираем все каталоги, в которых файлы для обучения
+        for src_dir in files_known:
+            if os.listdir(src + '/' + src_dir):
+                # сохраняем соответствие
+                codes[src_dir] = num
+                # назачаем новое число каждому образу
+                num += 1
+        result = round(net.activate(data)[0])
+        for filename in codes.keys():
+            if codes[filename] == result:
+                return u'<p>Распознано как</p><p><img src="images-src/%s" alt="Распознано как"></p>' % filename
+        return u'Не распознано'
+
+
+class Teach:
+    def GET(self):
+        with open('static/index.html', 'rb') as rb:
+            html = rb.read()
+        return html.replace('ip.gif', random.choice(os.listdir('static/images-learn')))
+
+    def POST(self):
+        img_str = web.input(name=['image']).get('image', 'data:image/png;base64,')
+        filename_src = web.input(name=['file']).get('file', None)
+        if not filename_src:
+            return u'Не сохранено'
+        data = base64.b64decode(img_str.replace('data:image/png;base64,', ''))
+
+        filename = 'static/images-learn/' + filename_src + '/'
+        suffix = datetime.now().strftime("%y%m%d_%H%M%S")
+        filename = "_".join([filename, suffix])
+
+        with open(filename + ".png", 'wb') as f:
+            f.write(data)
+        return u'Сохранено'
+
+
+class StaticFiles():
+    def GET(self, path):
+        static_path = ''.join((os.path.abspath(os.path.curdir), "/static"))
+        # print staticPath
+        for d, dirs, files in os.walk(static_path):
+            for f in files:
+                #realtive path to dir
+                directory = d.replace(static_path, '')
+                #realtive path to file
+                local_path = '/'.join([directory, f])[1:]
+                #file is found
+                if local_path == path:
+                    file_path = '/'.join([static_path, local_path])
+                    ext = file_path.split(".")[-1]
+                    image_types = {
+                        "png": "images/png",
+                        "jpg": "images/jpeg",
+                        "gif": "images/gif",
+                        "ico": "images/x-icon"
+                    }
+                    if ext in image_types:
+                        web.header("Content-Type", image_types[ext])
+                    return open(file_path).read()
+
+        #file is NOT found!
+        raise web.notfound("404 Not found!")
+
+    def POST(self, path):
+        return self.GET(path)
+
+
+# WSGIServer creating functions
+def run_gevent_server(app, ip, port=8080):
+    try:
+        from gevent.pywsgi import WSGIServer
+
+        WSGIServer((ip, port), app).serve_forever()
+        return True
+    except KeyboardInterrupt:  # If server stopped
+        return True  # don't show KeyboardInterrupt exception
+    except ImportError:  # If gevent module not found
+        return False  # resurn that server not started
+
+
+def run_simple_httpd_server(app, ip, port=8080):
+    from wsgiref.simple_server import make_server
+
+    make_server(ip, port, app).serve_forever()
+
+
+if __name__ == '__main__':
+    ip_adr = os.getenv('OPENSHIFT_PYTHON_IP') or '0.0.0.0'
+    bind_port = 18080 if os.sys.platform == 'win32' else 8080
+    # unsing web - web.py framework
+    application = web.application(urls, globals()).wsgifunc()
+
+    # Use gevent if we have it, otherwise run a simple httpd server.
+    print 'Starting WSGIServer on %s:%d ... ' % (ip_adr, bind_port)
+    if not run_gevent_server(application, ip_adr, bind_port):
+        print 'gevent probably not installed - using default simple server ...'
+        run_simple_httpd_server(application, ip_adr, bind_port)
+
+
+
+
+
+
+
+
+
---
+++ recognizer.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+
+__author__ = 'Odarchenko N.D.'
+
+import random
+import pickle
+import os
+from PIL import Image
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.datasets import ClassificationDataSet
+from pybrain.utilities import fListToString
+
+
+class Trainer(BackpropTrainer):
+    def trainUntilConvergence(self, dataset=None, maxEpochs=None, verbose=None, continueEpochs=10,
+                              validationProportion=0.25, maxError=None):
+        """Train the module on the dataset until it converges.
+
+        Return the module with the parameters that gave the minimal validation
+        error.
+
+        If no dataset is given, the dataset passed during Trainer
+        initialization is used. validationProportion is the ratio of the dataset
+        that is used for the validation dataset.
+
+        If maxEpochs is given, at most that many epochs
+        are trained. Each time validation error hits a minimum, try for
+        continueEpochs epochs to find a better one."""
+        epochs = 0
+        if dataset == None:
+            dataset = self.ds
+        if verbose == None:
+            verbose = self.verbose
+        # Split the dataset randomly: validationProportion of the samples for
+        # validation.
+        trainingData, validationData = (
+            dataset.splitWithProportion(1 - validationProportion))
+        if not (len(trainingData) > 0 and len(validationData)):
+            raise ValueError("Provided dataset too small to be split into training " +
+                             "and validation sets with proportion " + str(validationProportion))
+        self.ds = trainingData
+        bestweights = self.module.params.copy()
+        bestverr = self.testOnData(validationData)
+        trainingErrors = []
+        validationErrors = [bestverr]
+        while True:
+            trainingErrors.append(self.train())
+            validationErrors.append(self.testOnData(validationData))
+            if epochs == 0 or validationErrors[-1] < bestverr:
+                # one update is always done
+                bestverr = validationErrors[-1]
+                bestweights = self.module.params.copy()
+
+            if maxEpochs != None and epochs >= maxEpochs:
+                self.module.params[:] = bestweights
+                break
+            epochs += 1
+
+            if len(validationErrors) >= continueEpochs * 2:
+                # have the validation errors started going up again?
+                # compare the average of the last few to the previous few
+                old = validationErrors[-continueEpochs * 2:-continueEpochs]
+                new = validationErrors[-continueEpochs:]
+                if min(new) > max(old):
+                    self.module.params[:] = bestweights
+                    break
+            # остановка при достижении заданной точности
+            if not maxError is None and trainingErrors[-1] <= maxError:
+                self.module.params[:] = bestweights
+                break
+        trainingErrors.append(self.testOnData(trainingData))
+        self.ds = dataset
+        if verbose:
+            print 'train-errors:', fListToString(trainingErrors, 6)
+            print 'valid-errors:', fListToString(validationErrors, 6)
+        return trainingErrors, validationErrors
+
+
+def teach():
+    start_time = time.time()
+    # каталог с изображениями для обучения
+    src = 'static/images-learn'
+    files_known = os.listdir(src)
+
+    # размер изображений 100*100px
+    ds_sz = 100 * 100
+    # список для обучения
+    data_model = list()
+
+    # цифра на выходе
+    num = 0
+    # перебираем все каталоги, в которых файлы для обучения
+    for src_dir in files_known:
+        if os.listdir(src + '/' + src_dir):
+            # считываем все файлы, по которым учим сеть
+            for file_teach in os.listdir(src + '/' + src_dir):
+                im = Image.open(os.path.join(src, src_dir, file_teach))
+                data = [int(px != (0, 0, 0, 0)) for px in iter(im.getdata())]
+                # добавляем данные для обучения в виде списка
+                data_model.append([data, num])
+            # назачаем новое число каждому образу
+            num += 1
+
+    ds = ClassificationDataSet(ds_sz, nb_classes=num)
+    ds_test = ClassificationDataSet(ds_sz, nb_classes=num)
+
+    for inp, out in data_model:
+        ds.appendLinked(inp, [out])
+        ds_test.appendLinked(inp, [out])
+
+    # добавим больше данных для обучения
+    for n in xrange(500):
+        for i in range(num):
+            out = -1
+            inp = None
+            while out != i:
+                inp, out = random.choice(data_model)
+            ds.appendLinked(inp, [out])
+
+    net = buildNetwork(ds_sz, 2, 1)
+    trainer = Trainer(net, momentum=0.1, verbose=True, weightdecay=0.01)
+    trainer.setData(ds)
+    trainer.trainUntilConvergence(verbose=True, maxError=0.0125)
+    trainer.setData(ds_test)
+    trainer.testOnData(verbose=True)
+
+    print "--- %s seconds ---" % (int(time.time()) - int(start_time))
+
+    with open('trained.net', 'w') as fileObject:
+        pickle.dump(net, fileObject)
+
+
+if __name__ == '__main__':
+    teach()
+
